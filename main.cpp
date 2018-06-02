1aaae4567dc5c8b917f3dcefe19e00a916048c04
George Hosu <george.hosu@protonmail.com>
2018-06-02 13:00:39 +0100

    Trying to create the tcp client/server with boost

--- main.cpp
+++ main.cpp
@@ -1,16 +1,34 @@
 // A fast hash map, courtesy of https://github.com/skarupke
 #include "flat_hash_map/bytell_hash_map.hpp"
 
+// Other files from this project
+#include "networking.hpp"
+
 // stl
 #include <string>
 #include <vector>
 #include <thread>
+#include <stdexcept>
 
-// Linux specific stuff
-#include <sys/epoll.h>
+//boost
+#include <iostream>
+#include <boost/array.hpp>
+#include <boost/asio.hpp>
+using boost::asio::ip::tcp;
 
 //Debug purposes, remove for final compilation
 #include <iostream>
+
+#include <sys/epoll.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <netdb.h>
+#include <array>
 
 
 #define likely(x)      __builtin_expect(!!(x), 1)
@@ -35,14 +53,105 @@ template<class KeyT, class ValT> class Cache {
       }
       return nullptr;
     }
+
+    auto run() {
+      /* MAKE SERVER */
+
+      // Create the TCP server socket
+      const auto bind_res = networking::bind("5282");
+      if(bind_res.first != networking::Error::None) {
+        throw std::runtime_error("can't bind to port 5282 !");
+      }
+      const auto server_socket = bind_res.second;
+
+      // Make the socket non blocking
+      const auto err = networking::make_socket_nonblocking(server_socket);
+      if (err != networking::Error::None) {
+        throw std::runtime_error("Unprintable networking error !");
+      }
+
+      if (listen(server_socket, SOMAXCONN) == -1) {
+        throw std::runtime_error("Can't listen !");
+      }
+
+      int epoll_fd = epoll_create1(0);
+      if (epoll_fd == -1) {
+        throw std::runtime_error("epoll_create1 failed for a mystery reason, enjoy debugging this one !");
+      }
+
+      //Possible code duplication
+      struct epoll_event event;
+      event.data.fd = server_socket;
+      event.events = EPOLLIN | EPOLLET;
+
+      if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, server_socket, &event) == -1)
+      if (epoll_fd == -1)  {
+          throw std::runtime_error("epoll_ctl failed for a mystery reason, enjoy debugging this one !");
+      }
+
+      /* MAKE SERVER */
+
+
+      /* MAKE CLIENT */
+
+      auto conn_result = networking::connect("127.0.0.1", 5282);
+      if(conn_result.first != networking::Error::None) {
+        throw std::runtime_error("can't connect !!");
+      }
+      const auto client_socket = conn_result.second;
+
+      if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, client_socket, &event) == -1)
+      if (epoll_fd == -1)  {
+          throw std::runtime_error("epoll_ctl failed for a mystery reason, enjoy debugging this one !");
+      }
+
+      /* MAKE CLIENT */
+
+      constexpr int max_conn = 4096;
+      std::array<struct epoll_event, max_conn> events;
+
+
+      // Start the "server" loop
+      while (true) {
+        auto n = epoll_wait(epoll_fd, events.data(), max_conn, -1);
+        std::cout << n << std::endl;
+        for (int i = 0; i < n; i++) {
+          if (events[i].events & EPOLLERR || events[i].events & EPOLLHUP || !(events[i].events & EPOLLIN)) {
+                std::cerr << "Warning: epoll event error, closing connection !\n";
+                close(events[i].data.fd);
+          } else if (server_socket == events[i].data.fd) {
+            std::cout << "Accepting connections !" << std::endl;
+            // Block until connected... maybe bad ?
+            //while(networking::Error::None == networking::accept_conn(server_socket, event, epoll_fd)) {}
+            networking::accept_conn(server_socket, event, epoll_fd);
+          } else {
+            std::cout << "Reading data!" << std::endl;
+            auto fd = events[i].data.fd;
+            constexpr size_t buf_len = 512;
+            std::array<char,buf_len> arr_buf;
+            networking::read_data(fd, arr_buf.data(), buf_len);
+            std::cout << "Got character: " << arr_buf.data() << std::endl;
+            write(client_socket, "ABA", 3);
+          }
+        }
+      }
+    }
+
+    auto run2() {
+      boost::asio::io_service io_service;
+      tcp::acceptor acceptor(io_service, tcp::endpoint(tcp::v4(), 5282));
+      tcp::socket socket(io_service);
+    }
 };
 
 
 int main() {
   Cache<int64_t, std::string> cache_test{};
-  //
+  cache_test.run2();
+  /*
   ska::bytell_hash_map<int64_t, std::string> cache{};
   cache[12] = std::string{"test"};
   std::cout << "\"" << (*(cache.find(12) ) ).second  << "\"" << std::endl;
   return 0;
+  */
 }
--- networking.hpp
+++ networking.hpp
@@ -3,6 +3,8 @@
 #include <sys/epoll.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <iostream>
@@ -12,30 +14,157 @@
 #include <array>
 
 
-constexpr int32_t max_events = 4096
+
+// Based on this example: https://gist.github.com/dtoma/417ce9c8f827c9945a9192900aff805b
+//Courtesy of: Damien (https://github.com/dtoma)
+namespace networking {
+  enum class Error {
+    None = 0
+    ,Bind = 1
+    ,GetAddrInfo = 2
+    ,ERR_F_GETFL = 3
+    ,ERR_F_SETFL = 4
+    ,ERR_EAGAIN = 5 //No data
+    ,ERR_EWOULDBLOCK = 6 // Can't do w/o blocking thread
+    ,AcceptConn = 7
+    ,Epoll_ctl = 8
+    ,RemoteClosedConn = 9
+    ,Connect = 10
+  };
 
 
-enum class NwErr {
-  Ok
-  GetAddrInfo
-}
+  std::pair<Error, int> bind(const std::string & port) {
+    struct addrinfo hints;
+
+    memset(&hints, 0, sizeof(struct addrinfo));
+    hints.ai_family = AF_UNSPEC; /* Return IPv4 and IPv6 choices */
+    hints.ai_socktype = SOCK_STREAM; /* TCP */
+    hints.ai_flags = AI_PASSIVE; /* All interfaces */
+
+    struct addrinfo* result;
+    int sockt = getaddrinfo(nullptr, port.c_str(), &hints, &result);
+
+    if (sockt != 0) {
+        return {Error::GetAddrInfo, -1};
+    }
+
+    struct addrinfo* rp;
+    int socket_fd;
+    for(rp = result; rp != nullptr; rp = rp->ai_next) {
+      socket_fd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
+        if (socket_fd == -1) {
+            continue;
+        }
+
+        sockt = bind(socket_fd, rp->ai_addr, rp->ai_addrlen);
+        if (sockt == 0) {
+            break;
+        }
+
+        close(socket_fd);
+    }
+
+    if (rp == nullptr) {
+        return {Error::Bind, -1};
+    }
+
+    freeaddrinfo(result);
+
+    return {Error::None, socket_fd};;
+  };
 
 
-std::pair<NwErr, int> bind(const std::string & port) {
-  struct addrinfo hints;
+  // Open the file descriptor in non-blocking mode
+  Error make_socket_nonblocking(int socket_fd) {
+    // Get the flags set for the socket fd
+    int flags = fcntl(socket_fd, F_GETFL, 0);
+    if (flags == -1) {
+        return Error::ERR_F_GETFL;
+    }
 
-  memset(&hints, 0, sizeof(struct addrinfo));
-  hints.ai_family = AF_UNSPEC; /* Return IPv4 and IPv6 choices */
-  hints.ai_socktype = SOCK_STREAM; /* TCP */
-  hints.ai_flags = AI_PASSIVE; /* All interfaces */
+    flags = flags | O_NONBLOCK; //(I have no fucking idea what this bwor is doing)
+    // This command reset every other flags: no append, no async, no direct, no atime, and no nonblocking
+    int s = fcntl(socket_fd, F_SETFL, flags);
+    if (s == -1) {
+        return Error::ERR_F_SETFL;
+    }
 
-  struct addrinfo* result;
-  int sockt = getaddrinfo(nullptr, port.c_str(), &hints, &result);
+    return Error::None;
+  };
 
-  if (sockt != 0) {
-      return {NwErr::GetAddrInfo, -1};
+
+  Error accept_conn(int socket_fd, struct epoll_event& event, int epoll_fd) {
+      struct sockaddr in_addr;
+      socklen_t in_len = sizeof(in_addr);
+      int in_fd = accept(socket_fd, &in_addr, &in_len);
+
+      if (in_fd == -1) {
+        if (errno == EAGAIN) {
+          return Error::ERR_EAGAIN;
+        }
+        if (errno == EWOULDBLOCK) {
+          return Error::ERR_EWOULDBLOCK;
+        }
+        return Error::AcceptConn;
+      }
+
+      std::string hbuf(NI_MAXHOST, '\0');
+      std::string sbuf(NI_MAXSERV, '\0');
+
+      if (getnameinfo(&in_addr, in_len, const_cast<char*>(hbuf.data()), hbuf.size(),
+        const_cast<char*>(sbuf.data()), sbuf.size(), NI_NUMERICHOST | NI_NUMERICSERV) == 0) {
+          std::cout << "[I] Accepted connection on descriptor " << in_fd << "(host=" << hbuf << ", port=" << sbuf << ")" << "\n";
+      }
+
+      const auto err = make_socket_nonblocking(in_fd);
+      if (err != Error::None) {
+        return err;
+      }
+
+      event.data.fd = in_fd;
+      event.events = EPOLLIN | EPOLLET;
+
+      if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, in_fd, &event) == -1) {
+          return Error::Epoll_ctl;
+      }
+
+      return Error::None;
+  };
+
+
+  Error read_data(int fd, char * buf, size_t buf_len) {
+    auto count = read(fd, buf, buf_len);
+
+    if (count == -1) {
+        if (errno == EAGAIN) {
+            return Error::ERR_EAGAIN;
+        }
+    }
+
+    if (count == 0) {
+      close(fd);
+      return Error::RemoteClosedConn;
+    }
+
+    return Error::None;
+  };
+
+
+  std::pair<Error, int> connect(const char * addr, uint32_t port) {
+    struct sockaddr_in srv_addr;
+
+    auto socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    srv_addr.sin_family = AF_INET;
+    srv_addr.sin_addr.s_addr = inet_addr(addr);
+    srv_addr.sin_port = htons(port);
+
+    if (connect(socket_fd, (struct sockaddr *)&srv_addr, sizeof(srv_addr)) < 0) {
+      return {Error::Connect, -1};
+    }
+
+    make_socket_nonblocking(socket_fd);
+
+    return {Error::None, socket_fd};
   }
-
-  struct addrinfo* rp;
-  int socketfd;
 }
